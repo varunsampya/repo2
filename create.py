acd957274e84ea31d0b1eb010980bddb6e1dbd87
varunsampya <47588507+varunsampya@users.noreply.github.com>
2023-10-03 00:26:26 +0530

    Update create.py

--- create.py
+++ create.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 # Get the name and bundle name from the command line arguments.
-name = sys.argv[0]
+testname = sys.argv[0]
 bundle_name = sys.argv[1]
 
 # Print the name and bundle name.
-print(name)
+print(testname)
 print(bundle_name)
 
